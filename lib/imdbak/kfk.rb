06d081e2c7e60f01fe940a8aa92150e7c9fb16dd
ken <block24block@gmail.com>
2022-02-16 23:27:39 +0800

    distinguish internal and external port

--- lib/imdbak/kfk.rb
+++ lib/imdbak/kfk.rb
@@ -4,7 +4,7 @@ module Imdbak
   module Kfk
     class << self
       def client
-        @kafka ||= Kafka.new(["localhost:9092"], client_id: "imdbak")
+        @kafka ||= Kafka.new(["localhost:9093"], client_id: "imdbak")
       end
     end
   end
